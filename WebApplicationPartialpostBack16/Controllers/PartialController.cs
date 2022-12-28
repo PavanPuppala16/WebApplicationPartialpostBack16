d3f1bc17af962ab758bc35d30ec78fb71559ddbb
PavanPuppala16 <pavankumarp@fixitytech.com>
2022-12-28 20:05:46 +0530

    Add project files.

---
+++ WebApplicationPartialpostBack16/Controllers/PartialController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using WebApplicationPartialpostBack16.Models;
+using static WebApplicationPartialpostBack16.Models.DynamicRows;
+
+namespace WebApplicationPartialpostBack16.Controllers
+{
+    public class PartialController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.InfoModel = new List<Information>();
+            obj.InfoModel.Add(new Information { ID = 0, Name = "", Contact = 0, Address = "" });
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Index(EmployeeModel objmodel)
+        {
+            objmodel.InfoModel.Add(new Information { ID = 0, Name = "", Contact = 0, Address = "" });
+            return View(objmodel);
+        }
+    }
+}
+
+
---
+++ WebApplicationPartialpostBack16/Models/DynamicRows.Models.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace WebApplicationPartialpostBack16.Models
+{
+    public class DynamicRows
+    {
+        
+    }
+    public class EmployeeModel
+    {
+        public List<Information> InfoModel { get; set; }
+    }
+    public class Information
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Contact { get; set; }
+        public string Address { get; set; }
+    }
+}
